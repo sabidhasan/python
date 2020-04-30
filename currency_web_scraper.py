57d9e7a99196f26a82cc8915ec0c1bc217105880
celkington <celkington2216@gmail.com>
2020-04-30 17:24:09 +0100

    Update currency_web_scraper.py

--- currency_web_scraper.py
+++ currency_web_scraper.py
@@ -1,37 +1,38 @@
 '''
 ***All credit for data sourced for this project goes to exchangerates.org.uk***
 
-This program scrapes conversion rates between GBP(£) and some of the most 
-common currencies in the world. 
+This function scrapes conversion rates between GBP(£) and some of the most 
+common currencies in the world. The function takes one argument, gbp, that 
+must be either an integar or a float. 
 '''
 
-import requests
-from bs4 import BeautifulSoup
-URL = 'https://www.exchangerates.org.uk/'
-page = requests.get(URL,'html.parser')
-soup = BeautifulSoup(page.content, 'html.parser')
+def currency_func(gbp):
 
-panel = soup.find('div',class_='css-panes-sml')
-#grabs the desired div
-
-table = soup.find('table')
-#grabs the desired table
-
-eur_exchrate = table.find_all('td')[3].text
-usd_exchrate = table.find_all('td')[8].text
-nzd_exchrate = table.find_all('td')[13].text
-aud_exchrate = table.find_all('td')[18].text
-cad_exchrate = table.find_all('td')[23].text
-jpy_exchrate = table.find_all('td')[28].text
-zar_exchrate = table.find_all('td')[33].text
-aed_exchrate = table.find_all('td')[38].text
-inr_exchrate = table.find_all('td')[43].text
-try_exchrate = table.find_all('td')[48].text
-chf_exchrate = table.find_all('td')[53].text
-
-exchrate_list = [eur_exchrate, usd_exchrate, nzd_exchrate, aud_exchrate, 
-                 cad_exchrate, jpy_exchrate, zar_exchrate, aed_exchrate, 
-                 inr_exchrate, try_exchrate, chf_exchrate]
-
-
-print(exchrate_list)
+    import requests
+    from bs4 import BeautifulSoup
+    
+    URL = 'https://www.exchangerates.org.uk/'
+    page = requests.get(URL,'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    #grabbing the desired div and table for which the desired data is housed
+    table = soup.find('table')
+    
+    #grabbing all of the desired table data 
+    eur_exch = float(table.find_all('td')[3].text)
+    usd_exch = float(table.find_all('td')[8].text)
+    nzd_exch = float(table.find_all('td')[13].text)
+    aud_exch = float(table.find_all('td')[18].text)
+    cad_exch = float(table.find_all('td')[23].text)
+    jpy_exch = float(table.find_all('td')[28].text)
+    zar_exch = float(table.find_all('td')[33].text)
+    aed_exch = float(table.find_all('td')[38].text)
+    inr_exch = float(table.find_all('td')[43].text)
+    try_exch = float(table.find_all('td')[48].text)
+    chf_exch = float(table.find_all('td')[53].text)
+    
+    exchange_rates = [eur_exch, usd_exch, nzd_exch, aud_exch, cad_exch,
+                      jpy_exch, zar_exch, aed_exch, inr_exch, try_exch,
+                      chf_exch]
+    
+    return [gbp * x for x in exchange_rates]
